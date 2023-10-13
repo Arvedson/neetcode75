c80441d53bc87d78a3e5981d1ec476275d447b1f
Arvedson <tomasarvedson@gmail.com>
2023-10-12 21:53:45 -0600

    Parentesis valido

--- Valid Parantheses.js
+++ Valid Parantheses.js
@@ -1,33 +1,44 @@
-
-    
-
+/* este metodo tiene eficiencia en memoria/tiempo lineal O(n) 
+una iteracion, "dos campos de memoria"  */
 
 function parentesis (string){
 
     let stack = [];
-    
-    if(string.length ===0){
+
+    const pares = {
+
+        "[" : "]",
+        "{" : "}",
+        "(" : ")",
+
+    }
+
+    if(string.length === 0){
         return false
     }
 
+    if (string[0] === "}" || string[0] === "]" || string[0] === ")"){
+        return false
+    }
 
     for (let i = 0; i < string.length; i++){
-        if (string[i] === "}" || string[i] === "]" || string[i] === ")"){
-            return false
-        }
-        
-
-
 
         if (string[i] === "{" || string[i] === "[" || string[i] === "("){
             let current = string[i];
             stack.push(current)
-        }
 
+        } else {
+
+            const ultimoElemento = stack.pop()         /* accedemos al ultimo elemento del stack y simultaneamente lo eliminamos con .pop() */ 
+            if (string[i] !== pares[ultimoElemento]){   /* tomamos el ultimo elemento del stack como clave del objeto pares pares[ultimoElemento] "{([" y si es difernte al valor de i en especie "],},)" se rompe   */
+                return false
+            } 
+        }
     } 
+    return stack.length === 0
 }
 
 
 
-console.log(parentesis("({{}}"))
+console.log(parentesis("({[]}())"))
     
